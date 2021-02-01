b3067c587916e4c1ea5af74b08bafd0d7b817e26
Menelaos Alexiou <menelaos@Menelaos-iMac.local>
2021-02-01 20:33:33 +0200

    Inspired from example given v.1 (still not sure what was the purpose of this project)

--- script.js
+++ script.js
@@ -6,8 +6,17 @@ const theme1 = {
     thirdElement: ['everything', 'importand', 'my breath']
 };
 
-const theme2 = {};
-const theme3 = {};
+const theme2 = {
+    firstElement: ['bad luck', 'feeling good', 'strange mood'],
+    secondElement: ['try', 'train', 'waste'],
+    thirdElement: ['protesting', 'agrresivly', 'softly']
+};
+
+const theme3 = {
+    firstElement: ['spider', 'girlfried', 'boyfriend'],
+    secondElement: ['approching', 'diying', 'suffering'],
+    thirdElement: ['it', 'this', 'much']
+};
 
 // the functionalities
 
@@ -20,7 +29,26 @@ function sentenceCreator(objectsWithArrays) {
     const firstRandomWord = objectsWithArrays.firstElement[randomWords(objectsWithArrays.firstElement)];
     const secondRandomWord = objectsWithArrays.secondElement[randomWords(objectsWithArrays.secondElement)];
     const thirdRandomWord = objectsWithArrays.thirdElement[randomWords(objectsWithArrays.thirdElement)];
-    return `I ${firstRandomWord} you, beacause you are ${secondRandomWord} and ${thirdRandomWord} for me!`;
+    switch (objectsWithArrays) {
+        case theme1:
+            return `I ${firstRandomWord} you, beacause you are ${secondRandomWord} and ${thirdRandomWord} for me!`;
+            break;
+        case theme2:
+            return `When ${firstRandomWord} coming to you, never ${secondRandomWord} ${thirdRandomWord}!`;
+            break;
+        case theme3:
+            let wordAsArray = firstRandomWord.split('');
+            wordAsArray[0] = wordAsArray[0].toUpperCase()
+            let word1 = ''
+            wordAsArray.forEach(letter => {
+                 word1 += letter;               
+            });
+            return `${word1} is ${secondRandomWord}, I can't handle ${thirdRandomWord}.`;
+            break;
+    };
+        
 };
 
 console.log(sentenceCreator(theme1));
+console.log(sentenceCreator(theme2));
+console.log(sentenceCreator(theme3));
